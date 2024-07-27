e2bc37bbb2182e0ccca3ab3b72fc67e6257c203f
Duaa Tariq <tduaa34@gmail.com>
2024-07-27 13:16:57 -0400

    fixed

--- src/components/WeatherData.js
+++ src/components/WeatherData.js
@@ -6,41 +6,42 @@ import rainImage from '../wrainy.jpeg';
 import cloudsImage from '../wwcloudy.jpeg';
 import clearImage from '../wsunny.jpeg';
 
-
-const Weather = ({weatherData}) => {
-  
+const Weather = ({ weatherData }) => {
   const getImage = (weatherType) => {
-    switch(weatherType) {
+    switch (weatherType) {
       case 'Rain':
-        return rainImage; 
+        return rainImage;
       case 'Clouds':
-        return cloudsImage; 
+        return cloudsImage;
       case 'Clear':
-        return clearImage; 
+        return clearImage;
+      default:
+        return null; // Default case
     }
   };
-  
+
   return (
-        <ul className="weather-items-container" id ="weather-items-container">
-        {weatherData.map((item, i )=> {
-          return (
-            <React.Fragment key= {item.dt}>
-            {i % Math.floor(weatherData.length / 5) === 0 ? ( //ensures every 5th item is displayed
-                <li className= "weather-items">
-                  <p>{item.dt_txt}</p>
-                  <p>{item.weather[0].main}</p>
-                  <img src={getImage(item.weather[0].main)}/>
-                  <p>{item.main.temp} F</p>
-                  <p>wind: {item.wind.speed} mph</p>
-                  <p>humidity: {item.main.humidity}%</p>
-          </li>
-        ) : (
-          <></>
-        )}
-        </React.Fragment>
-      );
-        })}
-      </ul>
+    <ul className="weather-items-container" id="weather-items-container">
+      {weatherData.map((item, i) => {
+        return (
+          <React.Fragment key={item.dt}>
+            {i % Math.floor(weatherData.length / 5) === 0 ? ( // ensures every 5th item is displayed
+              <li className="weather-items">
+                <p>{item.dt_txt}</p>
+                <p>{item.weather[0].main}</p>
+                <img src={getImage(item.weather[0].main)} alt={item.weather[0].main} /> {/* Add meaningful alt text */}
+                <p>{item.main.temp} F</p>
+                <p>wind: {item.wind.speed} mph</p>
+                <p>humidity: {item.main.humidity}%</p>
+              </li>
+            ) : (
+              <></>
+            )}
+          </React.Fragment>
+        );
+      })}
+    </ul>
   );
 };
+
 export default Weather;
